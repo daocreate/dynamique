47e14f35f138062f32745d322247e541b448e051
daocreate <daocreate@gmail.com>
2020-02-26 09:07:48 +0100

    add global language file.

---
+++ resources/lang/en/controller.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'your_are_now_logged_out'                       => 'Your are now logged out!',
+    'please_add_on_fee:'                             => 'Please Add Atlest one Fee:',
+    'your_combination_was_incorrect'                 => 'Your email/password combination was incorrect',
+
+];
---
+++ resources/lang/fr/controller.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'your_are_now_logged_out'                       => 'Your are now logged out!',
+
+];
